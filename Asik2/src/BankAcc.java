da99fa5cc1392e647310988d2d8e8b1807602576
ABILMANSUR <pmmaratuly210507@gmail.com>
2026-04-10 23:09:01 +0500

    Asik2

---
+++ Asik2/src/BankAcc.java
@@ -0,0 +1,9 @@
+public class BankAcc {
+    String name;
+    double balance;
+
+    public BankAcc(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+}
